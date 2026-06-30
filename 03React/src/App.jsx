747012f41003370b0afe09a691cdc06788dd57eb
Adarsh Singh <adarshsinghhlm2004@gmail.com>
2026-06-30 12:55:29 +0530

    learn tailwind and props with project

---
+++ 03React/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Card from './components/card'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <Card username = "Adarsh"/>
+    <Card username="Abhishek"/>
+    </>
+  )
+}
+
+export default App
---
+++ 03React/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Card({username}) {
+
+  return (
+    <>      
+<a href="#" className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
+    <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">{username}</h5>
+    <p className="text-body">Here are the biggest technology acquisitions of 2025 so far, in reverse chronological order.</p>
+</a>
+    </>
+  );
+}
+
+export default Card;
