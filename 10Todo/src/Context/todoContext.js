aee63731404397cee4feef7bbabb05e83c8f213a
Adarsh Singh <adarshsinghhlm2004@gmail.com>
2026-07-08 05:37:35 +0530

    contextApi Todo

---
+++ 10Todo/src/Context/todoContext.js
@@ -0,0 +1,21 @@
+import { createContext,useContext } from "react";
+export const TodoContext = createContext({
+    //properties
+    todos:[
+        {
+            id:1,
+            todo:"Todo msg",
+            complete: false,
+        }
+    ],
+    // functionalities
+    addTodo:(todo) =>{},
+    updateTodo : (id,todo)=>{},
+    deleteTodo : (id)=>{},
+    toggleTodo : (id)=>{}  // toggleComplete
+
+})
+export const useTodo=()=>{
+    return useContext(TodoContext)
+}
+export const TodoProvider = TodoContext.Provider
